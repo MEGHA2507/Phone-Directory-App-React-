afddca3dcafbade8de704ebaeaae0884b0118aa6
MEGHA2507 <megha.banerjee07@gmail.com>
2022-01-25 02:13:26 +0530

    still event handlers in react

--- src/App.js
+++ src/App.js
@@ -1,51 +1,64 @@
+import { Component } from "react";
 import "./App.css";
 import Header from "./Header";
 
-function App() {
-  let subscribers = [
-    {
-      id: 1,
-      name: "Megha",
-      phone: "9999999999",
-    },
-    {
-      id: 2,
-      name: "Medha",
-      phone: "9736489969",
-    },
-    {
-      id: 3,
-      name: "Maa",
-      phone: "7777799999",
-    },
-    {
-      id: 4,
-      name: "Papa",
-      phone: "7777364679",
-    },
-  ];
+// function App() {
+class App extends Component {
+  deleteHandler(message, id) {
+    alert(message + " button " + id + " clicked");
+  }
+  render() {
+    let subscribers = [
+      {
+        id: 1,
+        name: "Megha",
+        phone: "9999999999",
+      },
+      {
+        id: 2,
+        name: "Medha",
+        phone: "9736489969",
+      },
+      {
+        id: 3,
+        name: "Maa",
+        phone: "7777799999",
+      },
+      {
+        id: 4,
+        name: "Papa",
+        phone: "7777364679",
+      },
+    ];
 
-  return (
-    <div>
-      <Header />
-      <div className="home-page">
-        <button className="add-btn">Add</button>
+    let phoneDirectory = "Phone Directory";
 
-        <div className="grid-container heading-container">
-          <span className="grid-item name-heading">Name</span>
-          <span className="grid-item phone-heading">Phone</span>
-        </div>
+    return (
+      <div>
+        <Header heading={phoneDirectory} />
+        <div className="home-page">
+          <button className="add-btn">Add</button>
 
-        {subscribers.map((sub) => {
-          return (
-            <div className="grid-container" key={sub.id}>
-              <span className="grid-item">{sub.name}</span>
-              <span className="grid-item">{sub.phone}</span>
-              <button className="grid-item delete-btn">Delete</button>
-            </div>
-          );
-        })}
-        {/* <div className="grid-container">
+          <div className="grid-container heading-container">
+            <span className="grid-item name-heading">Name</span>
+            <span className="grid-item phone-heading">Phone</span>
+          </div>
+
+          {subscribers.map((sub) => {
+            return (
+              <div className="grid-container" key={sub.id}>
+                <span className="grid-item">{sub.name}</span>
+                <span className="grid-item">{sub.phone}</span>
+                <button
+                  className="grid-item delete-btn"
+                  onClick={this.deleteHandler.bind(this, "Delete", sub.id)}
+                >
+                  Delete
+                </button>
+              </div>
+            );
+          })}
+          {/* <div className="grid-container">
           <span className="grid-item">Megha</span>
           <span className="grid-item">9999999999</span>
         </div>
@@ -53,9 +66,10 @@ function App() {
           <span className="grid-item">Medha</span>
           <span className="grid-item">7777777777</span>
         </div> */}
+        </div>
       </div>
-    </div>
-  );
+    );
+    // }
+  }
 }
-
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,9 @@
 import React from "react";
+//import { Component } from "react";
 import "./Header.css";
 
 // functional compoenent when we dont need to maintain states
-const Header = function () {
+const Header = function (props) {
   // const headerStyle = {
   //   textAlign: "center",
   //   padding: 20,
@@ -15,14 +16,14 @@ const Header = function () {
     // <div style={headerStyle}>
     //   Phone Directory
     // </div>
-    <div className="header">Phone Directory</div>
+    <div className="header">{props.heading}</div>
   );
 };
 
 // class component when we need to maintain states
 // class Header extends Component {
 //   render() {
-//     return <div className="header">Phone Directory</div>;
+//     return <div className="header">{this.props.heading}</div>;
 //   }
 // }
 
